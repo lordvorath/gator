8c4c6113446e398694d851f38670d0e3f4e35498
Stefano Trentin <4kill3@gmail.com>
2025-05-11 09:29:09 +0200

    initial postgres setup

--- cli.go
+++ cli.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/lordvorath/gator/internal/config"
+	"github.com/lordvorath/gator/internal/database"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -43,12 +48,39 @@ func (c *commands) register(name string, f func(*state, command) error) {
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) != 1 {
-		return fmt.Errorf("%v: inccorrect number of arguments: expected 1, got %d", cmd.name, len(cmd.args))
+		return fmt.Errorf("usage: %v <name>", cmd.name)
 	}
-	err := s.cfg.SetUser(cmd.args[0])
+	usr, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("user %v does not exist: %w", cmd.args[0], err)
+	}
+	err = s.cfg.SetUser(usr.Name)
 	if err != nil {
 		return err
 	}
 	fmt.Printf("User set to: %v\n", cmd.args[0])
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("usage: %v <name>", cmd.name)
+	}
+	newUser := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.args[0],
+	}
+	usr, err := s.db.CreateUser(context.Background(), newUser)
+	if err != nil {
+		return fmt.Errorf("error during user creation: %w", err)
+	}
+	err = s.cfg.SetUser(usr.Name)
+	if err != nil {
+		return fmt.Errorf("error updating config: %w", err)
+	}
+	fmt.Printf("User created successfully:\nUUID: %v\nCreated: %v\nUpdated: %v\nName: %v\n",
+		usr.ID, usr.CreatedAt, usr.UpdatedAt, usr.Name)
 	return nil
 }
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/lordvorath/gator/internal/config"
+	"github.com/lordvorath/gator/internal/database"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -12,12 +16,20 @@ func main() {
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	userState := state{cfg: &cfg}
+	db, err := sql.Open("postgres", cfg.DbUrl)
+	if err != nil {
+		log.Fatal("could not open database")
+	}
+	dbQueries := database.New(db)
+	userState := state{cfg: &cfg, db: dbQueries}
+
 	availableCommands := commands{commandMap: make(map[string]func(*state, command) error)}
 	availableCommands.register("login", handlerLogin)
+	availableCommands.register("register", handlerRegister)
+
 	args := os.Args
 	if len(args) < 2 {
-		log.Fatal("Not enough arguments")
+		log.Fatal("not enough arguments")
 	}
 	cmd := command{name: args[1], args: args[2:]}
 	err = availableCommands.run(&userState, cmd)
